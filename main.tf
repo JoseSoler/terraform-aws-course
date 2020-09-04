97e225a480711e9c91694580650e8227f3e0aad9
jlsoler <jose.soler@homyspace.com>
2020-09-04 17:01:44 +0200

    First exercise

---
+++ main.tf
@@ -0,0 +1,14 @@
+#First course exercise
+
+provider "aws" {
+  version = "2.65.0"
+  region  = "us-west-2"
+}
+  
+resource "aws_instance" "helloworld" {
+  ami           = "ami-09dd2e08d601bff67"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
